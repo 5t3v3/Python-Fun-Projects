b217a7bd1890df27c73c0a59291b2cfb52c33a5c
5t3v3 <41812621+Steveanonymous1@users.noreply.github.com>
2019-11-30 20:47:19 +0530

    Update redirection.py

--- redirection.py
+++ redirection.py
@@ -6,17 +6,18 @@ f_data=f.read().splitlines()      # Reading the file
 pay=input("\nEnter the payload text: ")
 p=open(str(pay),'r+')
 p_data=p.read().splitlines()
-print("\nStarting Test Module")
+print ("\nStarting Test Module")
 for i in f_data:
     for j in p_data:
         
         xy=str(j) # payload loop
-        xx=str(i)   #url loop
+        urll='https://'+i #adding http request
+        xx=str(urll)   #url loop
         r=xx+'/'+xy # concatenating url with payload
         req=requests.get(r) #http request (GET) with the combined string
-        print("\nThe request is ",r)
+        print ("\nThe request is ",r)
         out=req.status_code         # http response code of the request
-        print("Http Response code is : ",out)
+        print ("Http Response code is : ",out)
 
 
 p.close()
