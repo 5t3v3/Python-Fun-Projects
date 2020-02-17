f4bbe985325f78efcfb1f00f2b48e3e4ed48e773
5t3v3 <abhisteve123@gmail.com>
2020-02-17 17:32:28 +0530

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,13 +1,44 @@
+#####################################################
+#####      TERMINAL BASED GAME !!!!!!!!!!!!!!!  #####
+
 import random
 from sys import argv
 
-script, usr = argv
+#Enter the correct input or else ready to fail 
+script, usr = argv 
 opt=["rock","paper","scissor"]
-#usr=input("Enter the option r / p / s ")
 sy=random.randrange(0,3)
-
+y=opt[sy]
 if(usr==opt[sy]):
-    print("You won !!!!!!!!!!!!!!!")
+    print("Drawwwwwwwww   :|  ")
+
+
+
+elif(usr=="rock" and y=="paper"):
+    print("System wins !!!!")
+elif(y=="rock" and usr=="paper"):
+    print("User wins !!!")
+
+
+
+elif(y=="rock" and usr=="scissor"):
+    print("System wins !!!")
+elif(y=="scissor" and usr=="rock"):
+    print("User wins !!!!")
+
+
+    
+
+elif(y=="paper" and usr=="scissor"):
+    print("User wins !!!")
+elif(usr=="paper" and y=="scissor"):
+    print("System wins !!!!")
+
+
+
 else:
+    print(y)
+
     print("Bad luck....Try again :(")
 
+print(y)
